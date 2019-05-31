c0c83ac4e03d1f4f3d80a92c7236d599e52552e2
zlock <zlock88@gmail.com>
2019-05-31 14:26:29 +0800

    konfigurasi assets, modul jquery, angular.json package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngx-dbu-djpu';
+  title = 'SIMPUBU - DIREKTORAT BANDAR UDARA';
 }
