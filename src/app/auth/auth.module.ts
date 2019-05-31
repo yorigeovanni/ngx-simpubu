54b1f308272fdc1dd693627404edaf11bd02246a
zlock <zlock88@gmail.com>
2019-05-31 20:35:21 +0800

    customize content html

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { RessetPasswordComponent } from './component/resset-password/resset-password.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, RegisterComponent, RessetPasswordComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
