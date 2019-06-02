f9240403facd2e320ae9cc3be9d41209b9302609
zlock <zlock88@gmail.com>
2019-06-02 17:09:13 +0800

    customize login service

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,19 +1,20 @@
+// 01. INSERT KEBUTUHAN CORE MODULE
 import { Injectable } from '@angular/core';
 import { from } from 'rxjs';
 import { User } from '../models/user.model';
 
-
+// 02. INSERT KEBUTUHAN FIREBASE MODULE
 import * as firebase from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { AngularFirestore } from '@angular/fire/firestore';
 
-
+// 03. DEFINISI RUANG EKSEKUSI DI APP
 @Injectable({ providedIn: 'root' })
 
 
+// 04. MULAI EKSPORT SERVICE KE APP
 export class AuthService {
-
   constructor( private afAuth: AngularFireAuth, 
                private db: AngularFireDatabase,
                private firestore: AngularFirestore, ) 
@@ -24,6 +25,14 @@ export class AuthService {
   register(email: string, password: string) {
     return from(this.afAuth.auth.createUserWithEmailAndPassword(email, password));
   }
+
+
+  // SIMPAN USER
+  saveUser(user: User) {
+    const users = this.db.object('users/' + user.uid);
+    return users.set(user);
+  }
+
 
   //FUNCTION LOGIN DENGAN FORM
   login(email: string, password: string) {
@@ -48,9 +57,19 @@ export class AuthService {
     return from(this.afAuth.auth.signInWithPopup(provider));
   }
 
+
   //CHECK JIKA USER ADALAH ADMIN
   checkUserRole(uid: string) {
     return this.db.object('admins/' + uid).valueChanges();
+  }
+
+  
+  // FUNCTION UPDATE ONLINE STATUS - DIGUNAKAN PADA SAAT LOGIN DAN LOGOUT
+  updateOnlineStatus(uid: string, status: boolean) {
+    if (status) {
+      this.db.database.ref().child('users/' + uid).onDisconnect().update( { isOnline: false });
+    }
+    return from(this.db.object('users/' + uid).update({ isOnline: status }));
   }
 
 
@@ -58,6 +77,12 @@ export class AuthService {
   logout(uid: string) {
     this.updateOnlineStatus(uid, false);
     return from(this.afAuth.auth.signOut());
+  }
+
+
+  // GET CURRENT USER (DIGUNAKAN PADA UPDATE PROFILE DIBAWAH)
+  getCurrentUser() {
+    return this.afAuth.auth.currentUser;
   }
 
 
@@ -70,28 +95,9 @@ export class AuthService {
   }
 
 
-
-
-  saveUser(user: User) {
-    const users = this.db.object('users/' + user.uid);
-    return users.set(user);
-  }
-
-
-  updateOnlineStatus(uid: string, status: boolean) {
-    if (status) {
-      this.db.database.ref().child('users/' + uid).onDisconnect().update( { isOnline: false });
-    }
-    return from(this.db.object('users/' + uid).update({ isOnline: status }));
-  }
-
-  
-
   getAuthState() {
     return this.afAuth.authState;
   }
 
-  getCurrentUser() {
-    return this.afAuth.auth.currentUser;
-  }
+  
 }
