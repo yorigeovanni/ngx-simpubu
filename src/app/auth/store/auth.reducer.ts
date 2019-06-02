61c99853027e4621675717f46c7301b8ab31d220
zlock <zlock88@gmail.com>
2019-06-03 05:40:14 +0800

    update auth reduser, add new several selectors

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -5,9 +5,25 @@ export function authReducer(state = authInitialState, action: AuthAction): AuthS
   switch (action.type) {
     case AuthActionTypes.LOGIN_SUCCESS: {return Object.assign({}, state, { user: action.payload.user, isLoggedIn: true, isLoading: false, error: null });}
     case AuthActionTypes.UPDATE_PROFILE_SUCCESS: {return Object.assign({}, state, { user: action.payload.user, });}
-    
-    case AuthActionTypes.UPDATE_USER_ROLE: {return Object.assign({}, state, {isAdmin: action.payload.isAdmin['isAdmin']});}
 
+
+
+    case AuthActionTypes.UPDATE_USER_ROLE: {return Object.assign({}, state, {
+      isAdmin: action.payload.isAdmin['isAdmin'],
+      isAdminSatker: action.payload.isAdmin['isAdminSatker'],
+      isTeknisi: action.payload.isAdmin['isTeknisi'],
+
+      isBKP: action.payload.isAdmin['isBKP'],
+      isBTP: action.payload.isAdmin['isBTP'],
+      isDBU: action.payload.isAdmin['isDBU'],
+
+      isInspekturPeralatan: action.payload.isAdmin['isInspekturPeralatan'],
+      isInspekturPersonil: action.payload.isAdmin['isInspekturPersonil'],
+      isInspekturSatker: action.payload.isAdmin['isInspekturSatker'],
+      
+    });}
+
+    
     case AuthActionTypes.LOGIN_FAILED: { return Object.assign({}, state, { user: null, isLoading: false, isLoggedIn: false });}
     case AuthActionTypes.AUTH_ERROR: { return Object.assign({}, state, { error: action.payload.error });}
     case AuthActionTypes.LOGOUT_COMPLETED: { return Object.assign({}, state, { user: null, isLoading: false, isLoggedIn: false }); }
