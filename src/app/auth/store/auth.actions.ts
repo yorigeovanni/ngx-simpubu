bcd472ed2687408c6299a7f526d92e922a3a8f46
zlock <zlock88@gmail.com>
2019-06-03 00:25:53 +0800

    re-init ngrx/store -  auth action

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,42 +1,33 @@
-import { Action } from '@ngrx/store';
 import { User } from '../models/user.model';
+import { Action } from '@ngrx/store';
+
 
 export enum AuthActionTypes {
 
-
-  UPDATE_PROFILE = '[Auth] Update profile',
-  UPDATE_PROFILE_SUCCESS = '[Auth] Update profile success',
-
- 
+  SOCIAL_LOGIN = '[Auth] Social media login',
   LOGIN_SUCCESS = '[Auth] LOGIN Success',
   LOGIN_FAILED = '[Auth] LOGIN Failed',
 
-  SOCIAL_LOGIN = '[Auth] Social media login',
-
-  SAVE_USER = '[Auth] Save user',
-  UPDATE_ONLINE_STATUS = '[Auth] Update online status',
-
+  GET_USER = '[Auth] GET User',
   CHECK_USER_ROLE = '[Auth] Check user role',
   UPDATE_USER_ROLE = '[Auth] Update user role',
+  UPDATE_ONLINE_STATUS = '[Auth] Update online status',
 
-
-
-  GET_USER = '[Auth] GET User',
-
-  AUTH_ERROR = '[Auth] Error',
+  UPDATE_PROFILE = '[Auth] Update profile',
+  UPDATE_PROFILE_SUCCESS = '[Auth] Update profile success',
+  SAVE_USER = '[Auth] Save user',
 
   LOGOUT_REQUESTED = '[Auth] LOGOUT requested',
-  LOGOUT_COMPLETED = '[Auth] LOGOUT completed'
+  LOGOUT_COMPLETED = '[Auth] LOGOUT completed',
+  AUTH_ERROR = '[Auth] Error'
 
 }
 
 
-export class SaveUser implements Action {
-  readonly type = AuthActionTypes.SAVE_USER;
-  constructor(public payload: { user: User }) {}
+export class SocialLogin implements Action {
+  readonly type = AuthActionTypes.SOCIAL_LOGIN;
+  constructor(public payload: { authProvider: string }) {}
 }
-
-
 
 export class LoginSuccess implements Action {
   readonly type = AuthActionTypes.LOGIN_SUCCESS;
@@ -47,36 +38,35 @@ export class LoginFailed implements Action {
   readonly type = AuthActionTypes.LOGIN_FAILED;
 }
 
-export class SocialLogin implements Action {
-  readonly type = AuthActionTypes.SOCIAL_LOGIN;
-  constructor(public payload: { authProvider: string }) {}
-}
-
-
 export class GetUser implements Action {
   readonly type = AuthActionTypes.GET_USER;
 }
-
 
 export class CheckUserRole implements Action {
   readonly type = AuthActionTypes.CHECK_USER_ROLE;
   constructor(public payload: { uid: string }) {}
 }
 
+
 export class UpdateUserRole implements Action {
   readonly type = AuthActionTypes.UPDATE_USER_ROLE;
   constructor(public payload: { isAdmin: boolean }) {}
 }
 
+export class UpdateOnlineStatus implements Action {
+  readonly type = AuthActionTypes.UPDATE_ONLINE_STATUS;
+  constructor(public payload: { uid: string, status: boolean }) {}
+}
 
-
-
+export class SaveUser implements Action {
+  readonly type = AuthActionTypes.SAVE_USER;
+  constructor(public payload: { user: User }) {}
+}
 
 export class UpdateProfile implements Action {
   readonly type = AuthActionTypes.UPDATE_PROFILE;
   constructor(public payload: { displayName: string, photoUrl: string }) {}
 }
-
 
 export class UpdateProfileSuccess implements Action {
   readonly type = AuthActionTypes.UPDATE_PROFILE_SUCCESS;
@@ -93,13 +83,6 @@ export class LogoutCompleted implements Action {
   readonly type = AuthActionTypes.LOGOUT_COMPLETED;
 }
 
-
-export class UpdateOnlineStatus implements Action {
-  readonly type = AuthActionTypes.UPDATE_ONLINE_STATUS;
-  constructor(public payload: { uid: string, status: boolean }) {}
-}
-
-
 export class AuthError implements Action {
   readonly type = AuthActionTypes.AUTH_ERROR;
   constructor(public payload: { error: any }) {}
@@ -108,17 +91,16 @@ export class AuthError implements Action {
 
 
 export type AuthAction =
-
-  | UpdateProfile
-  | UpdateProfileSuccess
+  | SocialLogin
   | LoginSuccess
   | LoginFailed
-  | SocialLogin
-  | LogoutRequested
-  | LogoutCompleted
-  | SaveUser
-  | UpdateOnlineStatus
+  | GetUser
   | CheckUserRole
   | UpdateUserRole
-  | GetUser
+  | UpdateOnlineStatus
+  | UpdateProfile
+  | UpdateProfileSuccess
+  | SaveUser
+  | LogoutRequested
+  | LogoutCompleted
   | AuthError;
