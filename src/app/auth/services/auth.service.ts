0d3a7e48e62b1068b2bbb51ed76936bb8f89acf2
zlock <zlock88@gmail.com>
2019-06-02 16:55:26 +0800

    customize auth service and add AngularFirestore

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,32 +1,37 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
 import { from } from 'rxjs';
-import * as firebase from 'firebase/app';
-import { AngularFireDatabase } from '@angular/fire/database';
 import { User } from '../models/user.model';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+
+@Injectable({ providedIn: 'root' })
+
+
 export class AuthService {
 
-  constructor(private afAuth: AngularFireAuth, private db: AngularFireDatabase) { }
+  constructor( private afAuth: AngularFireAuth, 
+               private db: AngularFireDatabase,
+               private firestore: AngularFirestore, ) 
+  { }
 
+
+  // FUNCTION REGISTRASI
   register(email: string, password: string) {
     return from(this.afAuth.auth.createUserWithEmailAndPassword(email, password));
   }
 
-  updateProfile(displayName: string, photoUrl: string) {
-    const userProfile = this.afAuth.auth.currentUser;
-    if (userProfile) {
-      return <any>from(userProfile.updateProfile( { displayName: displayName, photoURL: photoUrl }));
-    }
-  }
-
+  //FUNCTION LOGIN DENGAN FORM
   login(email: string, password: string) {
     return from(this.afAuth.auth.signInWithEmailAndPassword(email, password));
   }
 
+
+  // LOGIN DENGAN VENDOR SOSIAL MEDIA
   socialLogin(authProvider: string) {
     let provider: any;
     if (authProvider === 'google') {
@@ -43,15 +48,35 @@ export class AuthService {
     return from(this.afAuth.auth.signInWithPopup(provider));
   }
 
+  //CHECK JIKA USER ADALAH ADMIN
+  checkUserRole(uid: string) {
+    return this.db.object('admins/' + uid).valueChanges();
+  }
+
+
+  // FUNCTION LOGOUT
   logout(uid: string) {
     this.updateOnlineStatus(uid, false);
     return from(this.afAuth.auth.signOut());
   }
 
+
+  //UPDATE PROFILE
+  updateProfile(displayName: string, photoUrl: string) {
+    const userProfile = this.afAuth.auth.currentUser;
+    if (userProfile) {
+      return <any>from(userProfile.updateProfile( { displayName: displayName, photoURL: photoUrl }));
+    }
+  }
+
+
+
+
   saveUser(user: User) {
     const users = this.db.object('users/' + user.uid);
     return users.set(user);
   }
+
 
   updateOnlineStatus(uid: string, status: boolean) {
     if (status) {
@@ -60,9 +85,7 @@ export class AuthService {
     return from(this.db.object('users/' + uid).update({ isOnline: status }));
   }
 
-  checkUserRole(uid: string) {
-    return this.db.object('admins/' + uid).valueChanges();
-  }
+  
 
   getAuthState() {
     return this.afAuth.authState;
