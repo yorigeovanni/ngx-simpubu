e3fcd2de9959a8965276204079faf7de1889a5cf
zlock <zlock88@gmail.com>
2019-06-18 03:37:17 +0800

    update kategori sisi udara service

--- src/app/master-data/master-data-peralatan/kategori-peralatan/sisi-udara/su-create/su-create.component.ts
+++ src/app/master-data/master-data-peralatan/kategori-peralatan/sisi-udara/su-create/su-create.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-su-create',
   templateUrl: './su-create.component.html',
   styleUrls: ['./su-create.component.css']
 })
+
 export class SuCreateComponent implements OnInit {
+
+  kategoriPeralatanForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.kategoriPeralatanForm = new FormGroup({
+      nama_fasilitas: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  get nama_fasilitas() { 
+    return this.kategoriPeralatanForm.get('nama_fasilitas'); 
+  }
+ 
+
+  onSubmit() {
+    if (this.kategoriPeralatanForm.valid) {
+      //this.store.dispatch(new actions.LoginRequested(this.loginForm.value));
+    }
   }
 
 }
