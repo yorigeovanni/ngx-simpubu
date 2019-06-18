d33d66c8e61cd534130bdfbd952634101f95f529
zlock <zlock88@gmail.com>
2019-06-18 16:25:52 +0800

    reconfigure scrollbar html

--- src/app/template/header/header.component.ts
+++ src/app/template/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component, HostListener,ViewChild, Renderer2, AfterViewInit, ElementRef, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import * as firebase from 'firebase/app';
+import { Component,  AfterViewInit, OnInit } from '@angular/core';
+import { Input, Output, EventEmitter, } from '@angular/core';
+import { HostListener, ViewChild } from '@angular/core';
+import { Renderer2, ElementRef } from '@angular/core';
+
 import { NavbarComponent } from 'ng-uikit-pro-standard';
 import { User } from './../../auth/models/user.model';
 
 import * as $ from 'jquery';
-import * as firebase from 'firebase/app';
+
 
 @Component({
   selector: 'app-header',
@@ -13,28 +18,23 @@ import * as firebase from 'firebase/app';
 
 export class HeaderComponent implements OnInit, AfterViewInit {
 
-  @ViewChild('nav') nav: NavbarComponent;
-
-  @Input() user: User;
-  @Input() isLoggedIn: boolean;
-  @Input() isLoading: boolean;
-  @Input() isAdmin: boolean;
-
-
+  @ViewChild('nav') nav : NavbarComponent;
+  @Input() user : User;
+  @Input() isLoggedIn : boolean;
+  @Input() isLoading : boolean;
+  @Input() isAdmin : boolean;
   @Output() logout = new EventEmitter<User>();
 
   constructor(
     private renderer: Renderer2, 
-    private el: ElementRef)
+    private element_html: ElementRef)
      { }
 
   
-
-
   @HostListener('click', ['$event'])
   onClick(event) {
-    const toggler = this.el.nativeElement.querySelector('.navbar-toggler');
-    const togglerIcon = this.el.nativeElement.querySelector('.navbar-toggler-icon');
+    const toggler = this.element_html.nativeElement.querySelector('.navbar-toggler');
+    const togglerIcon = this.element_html.nativeElement.querySelector('.navbar-toggler-icon');
       if (event.target === toggler || event.target === togglerIcon) {
       console.log('test');
       setTimeout(() => {
@@ -54,21 +54,19 @@ export class HeaderComponent implements OnInit, AfterViewInit {
       this.transformDropdowns();
   }
 
-  
   ngAfterViewInit() {
   this.transformDropdowns();
   }
 
   transformDropdowns() {
-    const dropdownMenu = Array.from(this.el.nativeElement.querySelectorAll('.dropdown-menu'));
+    const dropdownMenu = Array.from(this.element_html.nativeElement.querySelectorAll('.dropdown-menu'));
+    console.log(dropdownMenu);
     const navHeight = this.nav.navbar.nativeElement.clientHeight + 'px';
       
     dropdownMenu.forEach((dropdown) => {
       this.renderer.setStyle(dropdown, 'transform', `translateY(${navHeight})`);
     });
   }
-
-
 
   ngOnInit() {
     $(".online-span").hide();
